bdca410d854b7b09fbe7cbd7fccfe6c5333b5648
Karlis <karlis.tiltins89@gmail.com>
2022-03-09 13:14:05 +0200

    array update 09.03

--- src/main/java/io/codelex/arrays/practice/Hangman.java
+++ src/main/java/io/codelex/arrays/practice/Hangman.java
@@ -7,25 +7,44 @@ public class Hangman {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] listOfWords = {"building", "river", "road"};
+        String guessedLetters = "";
+        String missedLetters ="";
+        String chooseWord = listOfWords[(int) (Math.random() * listOfWords.length)];
 
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
-        System.out.println();
-        System.out.println("Word: ");
-        hiddenWord(listOfWords);
-        System.out.println();
-        System.out.println("Misses:");
-        char[] missedWords = {};
-        System.out.println();
-        System.out.println("Guess: ");
+        while(true){
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+            System.out.println();
+            System.out.println("Word: ");
+            printOutWord(chooseWord, guessedLetters);
+            System.out.println();
+            System.out.println("Misses:");
 
-        char inputChar = scan.next().charAt(0);
-        int tries = 10;
+            System.out.println();
+            System.out.println("Guess: ");
+
+            char inputChar = scan.nextLine().charAt(0);
+            int tries = 10;
+
+
+            if( chooseWord.contains(String.valueOf(inputChar))) {
+                guessedLetters += inputChar;
+            } else {
+                missedLetters +=inputChar;
+            }
+
+        }
+
+
     }
-    public static void hiddenWord(String[] words) {
-        String chooseWord = words[(int) (Math.random() * words.length)];
-        char[] wordToLetters = chooseWord.toCharArray();
-        for (char i = 0; i < wordToLetters.length; i++) {
-            System.out.print( wordToLetters[i] = '_');
+
+
+    public static void printOutWord(String word, String guessedLetters) {
+        for (int i = 0; i < word.length(); i++) {
+            if (guessedLetters.contains(String.valueOf(word.charAt(i)))){
+                System.out.print(word.charAt(i)+" ");
+            } else{
+                System.out.print("_ ");
+            }
         }
     }
 }
