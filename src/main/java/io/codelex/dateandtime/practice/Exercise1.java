0809b800f9c6f9c33e169f2b59c44866ccae68bc
Karlis <karlis.tiltins89@gmail.com>
2022-03-16 20:30:38 +0200

    dateAndTime 16.03

--- src/main/java/io/codelex/dateandtime/practice/Exercise1.java
+++ src/main/java/io/codelex/dateandtime/practice/Exercise1.java
@@ -1,44 +1,47 @@
-//package io.codelex.dateandtime.practice;
-//
-//import javax.sound.midi.Soundbank;
-//import java.text.DateFormat;
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDate;
-//import java.time.format.DateTimeFormatter;
-//import java.time.temporal.ChronoUnit;
-//import java.util.Calendar;
-//import java.util.Date;
-//import java.util.Locale;
-//import java.util.Scanner;
-//
-//public class Exercise1 {
-//    public static void main(String[] args) throws ParseException {
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Enter starting date (time format:dd/mm/yyyy)");
-//        String startingDate = scan.nextLine();
-//        DateTimeFormatter dateFormatOne = DateTimeFormatter.ofPattern("d/M/yyyy");
-//        LocalDate start = LocalDate.parse(startingDate, dateFormatOne);
-//
-//        System.out.println("Enter last work day (time format:dd/mm/yyyy)");
-//        String lastWorkingDate = scan.nextLine();
-//        DateTimeFormatter dateFormatTwo = DateTimeFormatter.ofPattern("d/M/yyyy");
-//        LocalDate end = LocalDate.parse(startingDate, dateFormatOne);
-//
-//        Calendar cal1 = Calendar.getInstance();
-//        Calendar cal2 = Calendar.getInstance();
-//        cal1.setTime(start);
-//        cal2.setTime(date2);
-//
-//
-//        int numberOfDays = 0;
-//        while (start.before(end)) {
-//            if ((Calendar.SATURDAY != start.get(Calendar.DAY_OF_WEEK))
-//                    &&(Calendar.SUNDAY != cal1.get(Calendar.DAY_OF_WEEK))) {
-//                numberOfDays++;
-//            }
-//            cal1.add(Calendar.DATE,1);
-//    }
-//
-//
-//}
+package io.codelex.dateandtime.practice;
+
+import java.text.ParseException;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+public class Exercise1 {
+    public static void main(String[] args) throws ParseException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter starting date (time format:dd/mm/yyyy)");
+        String startingDate = scan.nextLine();
+        DateTimeFormatter dateFormatOne = DateTimeFormatter.ofPattern("d/M/yyyy");
+        LocalDate start = LocalDate.parse(startingDate, dateFormatOne);
+
+        System.out.println("Enter last work day (time format:dd/mm/yyyy)");
+        String lastWorkingDate = scan.nextLine();
+        DateTimeFormatter dateFormatTwo = DateTimeFormatter.ofPattern("d/M/yyyy");
+        LocalDate end = LocalDate.parse(lastWorkingDate, dateFormatTwo);
+
+        int startW = start.getDayOfWeek().getValue();
+        int endW = end.getDayOfWeek().getValue();
+
+        long days = ChronoUnit.DAYS.between(start, end);
+        long result = days - 2 * (days / 7);
+        long hoursInDays = 8;
+
+        if (days % 7 != 0) {
+            if (startW == 7) {
+                result -= 1;
+            } else if (endW == 7) {
+                result -= 1;
+            } else if (endW < startW) {
+                result -= 2;
+            }
+        }
+        long daysToHours = result * hoursInDays;
+        System.out.println("Employee worked " + daysToHours + " hours in this period ");
+    }
+
+}
+
+
+
