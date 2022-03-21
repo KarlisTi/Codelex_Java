cbf0ca9789d540ab94fa478b335993fbed0c3b05
Karlis <56352422+KarlisTi@users.noreply.github.com>
2022-03-21 20:53:28 +0200

    BankAccountMainChanges
    Nomainīju clases nosaukumu

--- src/main/java/io/codelex/classesandobjects/practice/Exercise10/BankAccountMain.java
+++ src/main/java/io/codelex/classesandobjects/practice/Exercise10/BankAccountMain.java
@@ -1,6 +1,6 @@
 package io.codelex.classesandobjects.practice.Exercise10;
 
-public class BankAccount_main {
+public class BankAccountMain {
     public static void main(String[] args) {
         BankAccount account = new BankAccount("Janis", 17.25);
 
