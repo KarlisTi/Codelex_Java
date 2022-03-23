adea74bff6db833e217e6ae8ef6c63f80a1dd659
Karlis <56352422+KarlisTi@users.noreply.github.com>
2022-03-23 10:56:49 +0200

    Update movie

--- src/main/java/io/codelex/classesandobjects/practice/Exercise4/Movie_test.java
+++ src/main/java/io/codelex/classesandobjects/practice/Exercise4/Movie_test.java
@@ -2,11 +2,15 @@ package io.codelex.classesandobjects.practice.Exercise4;
 
 public class Movie_test {
     public static void main(String[] args) {
+            List<Movie> mov = new ArrayList<>();
+        Movie newMovie = new Movie(mov);
         Movie firstMovie = new Movie("Casino Royale", "Eon Productions", "PGSHY13");
         Movie secondMovie = new Movie("Glass", "Vista International", "PGSHY13");
         Movie thirdMovie = new Movie("Spider-man: Into the Spider-Verse", "Columbia Pictures", "PG");
-
-
+        mov.add(firstMovie);
+        mov.add(secondMovie);
+        mov.add(thirdMovie);
+        System.out.println(newMovie.getPG(mov));
     }
 }
 
