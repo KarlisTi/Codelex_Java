01c6c23f30ff2d413923d6de7ff5e8419cd01584
KarlisTi <karlis.tiltins89@gmail.com>
2022-03-25 11:06:19 +0200

    Excetions update 25.03

--- src/main/java/io/codelex/Exceptions/practice/Exercise4/Exercise4.java
+++ src/main/java/io/codelex/Exceptions/practice/Exercise4/Exercise4.java
@@ -1,33 +1,28 @@
-package io.codelex.Exceptions.practice;
-
-import java.util.Scanner;
+package io.codelex.Exceptions.practice.Exercise4;
 
 public class Exercise4 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         try {
             double d = getInput(args[0]);
             double root = Math.sqrt(d);
             System.out.println(
                     "The square root of " + d + " is " + root);
+
         } catch (NumberFormatException e) {
             System.out.println("Be sure to enter a number.");
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Enter number as first parameter.");
+        } catch (NonPositiveNumberException e) {
+            System.out.println(e.getMessage());
         }
-        // catch (NonPositiveNumberException e) {
         System.out.println("Result will be imaginary number.");
     }
 
 
-    static double getInput(String s) {
-        double d = new Double(s).doubleValue();
-        try {
-            if (d < 0) {
-                throw new IllegalArgumentException("Enter a postive number");
-            }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
+    static double getInput(String s) throws NonPositiveNumberException {
+        double d = Double.parseDouble(s);
+        if (d < 0) {
+            throw new NonPositiveNumberException();
         }
         return d;
     }
