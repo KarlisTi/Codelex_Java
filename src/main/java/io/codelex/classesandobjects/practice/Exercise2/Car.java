3534c57883dd3a961e28dcd3085f263ed349e9b4
KarlisTi <karlis.tiltins89@gmail.com>
2022-03-29 11:10:28 +0300

    Polymorphism update 29.03

--- src/main/java/io/codelex/classesandobjects/practice/Exercise2/Car.java
+++ src/main/java/io/codelex/classesandobjects/practice/Exercise2/Car.java
@@ -1,54 +1,54 @@
-package io.codelex.classesandobjects.practice.Exercise2;
-
-public class Car {
-    
-    private double startKilometers;
-    private double endKilometers;
-    private double liters;
-
-    
-    public Car(double startKilometers, double endKilometers, double liters) {
-    this.startKilometers = startKilometers;
-    this.endKilometers = endKilometers;
-    this.liters = liters;
-    }
-    
-    public double getStartKilometers() {
-        return startKilometers;
-    }
-
-    public void setStartKilometers(double startKilometers) {
-        this.startKilometers = startKilometers;
-    }
-
-    public double getEndKilometers() {
-        return endKilometers;
-    }
-
-    public void setEndKilometers(double endKilometers) {
-        this.endKilometers = endKilometers;
-    }
-
-    public double getLiters() {
-        return liters;
-    }
-
-    public void setLiters(double liters) {
-        this.liters = liters;
-    }
-
-    public static double calculateConsumption(double start,double end, double liters) {
-           return (end - start)* liters;
-        }
-        public static boolean gasHog(double valueA){
-            double literPerKillometers = 15.0
-            return valueA > litterPerKillometers;
-        }
-        public static boolean economyCar(double valueB){
-             double literPerKillometers = 5.0
-            return valueB < literPerKillometers;
-        }
-
-    }
-
-
+//package io.codelex.classesandobjects.practice.Exercise2;
+//
+//public class Car {
+//
+//    private double startKilometers;
+//    private double endKilometers;
+//    private double liters;
+//
+//
+//    public Car(double startKilometers, double endKilometers, double liters) {
+//    this.startKilometers = startKilometers;
+//    this.endKilometers = endKilometers;
+//    this.liters = liters;
+//    }
+//
+//    public double getStartKilometers() {
+//        return startKilometers;
+//    }
+//
+//    public void setStartKilometers(double startKilometers) {
+//        this.startKilometers = startKilometers;
+//    }
+//
+//    public double getEndKilometers() {
+//        return endKilometers;
+//    }
+//
+//    public void setEndKilometers(double endKilometers) {
+//        this.endKilometers = endKilometers;
+//    }
+//
+//    public double getLiters() {
+//        return liters;
+//    }
+//
+//    public void setLiters(double liters) {
+//        this.liters = liters;
+//    }
+//
+//    public static double calculateConsumption(double start,double end, double liters) {
+//           return (end - start)* liters;
+//        }
+//        public static boolean gasHog(double valueA){
+//            double literPerKillometers = 15.0
+//            return valueA > litterPerKillometers;
+//        }
+//        public static boolean economyCar(double valueB){
+//             double literPerKillometers = 5.0
+//            return valueB < literPerKillometers;
+//        }
+//
+//    }
+//
+//
---
+++ src/main/java/io/codelex/polymorphism/practice/exercise1/Porche.java
@@ -0,0 +1,30 @@
+package io.codelex.polymorphism.practice.exercise1;
+
+public class Porche implements Car {
+
+    private Integer currentSpeed = 0;
+
+    public Porche() {
+
+    }
+
+    @Override
+    public void speedUp() {
+        currentSpeed += 12;
+    }
+
+    @Override
+    public void slowDown() {
+        currentSpeed += 12;
+    }
+
+    @Override
+    public String showCurrentSpeed() {
+        return currentSpeed.toString();
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Rrrrrrr.....");
+    }
+}
