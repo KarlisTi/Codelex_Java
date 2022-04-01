938bb182b4079be33550110ebbe8fceb0911bbe0
Karlis <56352422+KarlisTi@users.noreply.github.com>
2022-04-01 10:48:55 +0300

    Update exercise

--- src/main/java/io/codelex/Exceptions/practice/Exercise1.java
+++ src/main/java/io/codelex/Exceptions/practice/Exercise1.java
@@ -4,17 +4,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercise1 {
+ public class Exercise1 {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Enter the numerator");
-            String numerator = scanner.next();
-            System.out.println("Enter the divisor");
-            int divisor = scanner.nextInt();
-            int result;
-
+        boolean programIsRunning = true;
+        while (programIsRunning) {
             try {
+                System.out.println("Enter the numerator");
+                String numerator = scanner.next();
+                System.out.println("Enter the divisor");
+                int divisor = scanner.nextInt();
+                int result;
                 int num = Integer.parseInt(numerator);
                 result = num / divisor;
                 System.out.println(numerator + " / " + divisor + " = " + result);
@@ -23,6 +23,11 @@ public class Exercise1 {
             } catch (NumberFormatException e) {
                 System.out.println("You entered bad data.");
                 System.out.println("Please try again");
+            } catch (IllegalStateException e) {
+                System.out.println("Program is closed");
+            } finally {
+                scanner.close();
+                programIsRunning = false;
             }
         }
     }
