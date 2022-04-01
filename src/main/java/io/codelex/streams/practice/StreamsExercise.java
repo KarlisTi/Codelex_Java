21aef212f1fa6ca0d7058d1d08e19d1734c1d999
KarlisTi <karlis.tiltins89@gmail.com>
2022-04-01 07:16:10 +0300

    streams update 01.04

---
+++ src/main/java/io/codelex/streams/practice/StreamsExercise.java
@@ -0,0 +1,134 @@
+package io.codelex.streams.practice;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Nodes.collect;
+
+
+/**
+ * TODO - search for StreamsExerciseTest and make all tests pass
+ */
+public class StreamsExercise {
+    public static List<Integer> returnSquareRoot(List<Integer> numbers) {
+        return numbers.stream().map((Integer n) -> (int) Math.sqrt(n)).collect(Collectors.toList());
+    }
+
+    public static List<Integer> getAgeFromUsers(List<User> user) {
+        return user.stream().map(User::getAge).collect(Collectors.toList());
+    }
+
+    public static List<Integer> getDistinctAges(List<User> users) {
+        return users.stream().map(User::getAge).distinct().collect(Collectors.toList());
+    }
+
+    public static List<User> getLimitedUserList(List<User> users, int limit) {
+        return users.stream().limit(limit).collect(Collectors.toList());
+    }
+
+    public static Integer countUsersOlderThen25(List<User> users) {
+        return Math.toIntExact(users.stream().filter((User n) -> n.getAge() > 25).count());
+    }
+
+    public static List<String> mapToUpperCase(List<String> strings) {
+        return strings.stream().map(String::toUpperCase).collect(Collectors.toList());
+    }
+
+    public static List<Integer> sum(List<Integer> integers) {
+        return integers.stream().map((Integer
+
+    public static List<Integer> skip(List<Integer> integers, Integer toSkip) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static List<String> getFirstNames(List<String> names) {
+        return names;
+    }
+
+    public static List<String> getDistinctLetters(List<String> names) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    public static String separateNamesByComma(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static double getAverageAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Integer getMaxAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Integer getMinAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<Boolean, List<User>> partionUsersByGender(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<Integer, List<User>> groupByAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<Boolean, Map<Integer, List<User>>> groupByGenderAndAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Map<Boolean, Long> countGender(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean anyMatch(List<User> users, int age) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean noneMatch(List<User> users, int age) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Optional<User> findAny(List<User> users, String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static List<User> sortByAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Stream<Integer> getBoxedStream(IntStream stream) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static List<Integer> generateFirst10PrimeNumbers() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean isPrime(int number) {
+        return IntStream.rangeClosed(2, number / 2).noneMatch(i -> number % i == 0);
+    }
+
+    public static List<Integer> generate10RandomNumbers() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static User findOldest(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int sumAge(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static IntSummaryStatistics ageSummaryStatistics(List<User> users) {
+        throw new UnsupportedOperationException();
+    }
+}
